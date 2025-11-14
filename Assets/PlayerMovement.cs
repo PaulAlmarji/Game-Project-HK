66e4873e38beccb829a76de0de977452e0cfd3d8
AHMAD ISTAITIYA <ahmadist2006@gmail.com>
2025-11-14 23:55:24 +0300

    movement shit

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -2,15 +2,25 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+
+    float horizontal;
+    float moveSpeed = 5f;
+    Rigidbody2D rb;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        rb = GetComponents<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        horizontal = Input.GetAxis("Horizontal");
+    }
+
+    private void fixedUpdate()
+    {
+        rb.velocity = new Vector2(horizontal * moveSpeed, rb.velocity.y);
     }
 }
