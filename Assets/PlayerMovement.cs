008e5144be1b0f7dfebf9e12fea1b4160b61d76d
PaulAlmarji <paulalmarji@gmail.com>
2025-11-15 00:32:19 +0300

    Ladies and Gents The player Moves

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -5,22 +5,51 @@ public class PlayerMovement : MonoBehaviour
 
     float horizontal;
     float moveSpeed = 5f;
+    bool isFacingRight = true;
+    float jumpingPower = 5f;
+    bool isJumping = false;
+    
     Rigidbody2D rb;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        rb = GetComponents<Rigidbody2D>();
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
         horizontal = Input.GetAxis("Horizontal");
+
+        if(Input.GetButtonDown("Jump") && !isJumping)
+        {
+            rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpingPower);
+            isJumping = true;
+        }
+
+        Flip();
     }
 
-    private void fixedUpdate()
+    private void FixedUpdate()
     {
         rb.linearVelocity = new Vector2(horizontal * moveSpeed, rb.linearVelocity.y);
+    }
+
+    void Flip()
+    {
+        if(isFacingRight && horizontal < 0 || !isFacingRight && horizontal > 0)
+        {
+            isFacingRight = !isFacingRight;
+            Vector3 scale = transform.localScale;
+            scale.x *= -1f;
+            transform.localScale = scale;
+        }
+        
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        isJumping = false;
     }
 }
