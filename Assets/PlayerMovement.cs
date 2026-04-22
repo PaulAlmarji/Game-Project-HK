6e5dac18185be6c79a4e0d995fcf45830d8709bd
AHMAD ISTAITIYA <ahmadist2006@gmail.com>
2026-04-22 14:43:57 +0300

    paul mid

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -45,7 +45,7 @@ public class PlayerMovement : MonoBehaviour
             scale.x *= -1f;
             transform.localScale = scale;
         }
-        //hiiiiiiiiiiiiiiiiiiiii yurrrrrrrrr
+        //hiiiiiiiiiiiiiiiiiiiii yurrrrrrrrr paulllllll
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
